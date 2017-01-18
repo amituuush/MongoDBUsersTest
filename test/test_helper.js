0879c3fe1568a30486f3664ad5341310717cbd34
Amit Ranan <ranan.amit@gmail.com>
2017-01-18 15:12:25 -0800

    create beforeEach function and drop users before each test

--- test/test_helper.js
+++ test/test_helper.js
@@ -11,3 +11,16 @@ mongoose.connection
   .on('error', (error) => {
     console.warn('Warning', error);
   });
+
+// runs before each test in our test suite
+// delete all records in our users collections before each test
+// every function that we run that we run inside of mocha (beforeEach, it, describe, etc) gets called with a 'done' callback.
+// Done says when you run the done function, it means I'm done with all the logic inside the beforeEach
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // callback is run only after our collection of users is deleted or 'dropped'
+    done();
+  });
+  // mocha will continue to run tests and were not sure if the drop is completed before next test
+
+});
