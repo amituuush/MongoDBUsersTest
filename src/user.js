1db8b7c27531cfa9d63724c6de93b932e816e54d
Amit Ranan <ranan.amit@gmail.com>
2017-01-18 13:34:51 -0800

    create users.js file and import statments

---
+++ src/user.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// this allows us to create a schema for our use model.
+// schema says 'I expect model to have properties and the data we expect those properties to be'
+
+// Model represents all of the data in a single collection in our mongo database
+// once we create the model, mongoose will create a collection in our data
