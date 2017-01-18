e7e0c76e863ee63c1067e1b5991c0a5fd4b25327
Amit Ranan <ranan.amit@gmail.com>
2017-01-18 15:22:30 -0800

    add assert to saving user test and enhance mongo connection to account for case when tests run before we connect to db, use es6 promises

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
-// connects mongoose to mongodb
-// mongo will connect to db called /users-test
-// we don't have to create the db ahead of time, mongo and mongoose will create the db for you using this line
-mongoose.connect('mongodb://localhost/users_test');
+mongoose.Promise = global.Promise;
 
-// takes time to connect to mongo database, so we use these event handlers to handle these events once there is or isn't a connection
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+// runs one time for entire test suite
+before((done) => {
+  // connects mongoose to mongodb
+  // mongo will connect to db called /users-test
+  // we don't have to create the db ahead of time, mongo and mongoose will create the db for you using this line
+  mongoose.connect('mongodb://localhost/users_test');
+
+  // takes time to connect to mongo database, so we use these event handlers to handle these events once there is or isn't a connection
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
+
 
 // runs before each test in our test suite
 // delete all records in our users collections before each test
