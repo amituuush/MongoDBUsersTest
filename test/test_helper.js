428022f3ab61a0f2f67e12667f520d386d8bf647
Amit Ranan <ranan.amit@gmail.com>
2017-01-18 13:25:50 -0800

    add explanations to mongoose connect

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
+// connects mongoose to mongodb
+// mongo will connect to db called /users-test
+// we don't have to create the db ahead of time, mongo and mongoose will create the db for you using this line
 mongoose.connect('mongodb://localhost/users_test');
+
+// takes time to connect to mongo database, so we use these event handlers to handle these events once there is or isn't a connection
 mongoose.connection
   .once('open', () => console.log('Good to go!'))
   .on('error', (error) => {
