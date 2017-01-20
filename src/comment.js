a1fbf6626708fae96dfc2b24c53cf458d805e48e
Amit Ranan <ranan.amit@gmail.com>
2017-01-20 15:33:20 -0800

    complete BlogPost model

--- src/comment.js
+++ src/comment.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const BlogPostSchema = new Schema({
-  title: String,
+const CommentSchema = new Schema({
   content: String,
-  
+  user: 
 });
