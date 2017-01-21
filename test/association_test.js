4217ef67fb9d4f42ddc4617257c85feb7449bd29
Amit Ranan <ranan.amit@gmail.com>
2017-01-20 16:04:25 -0800

    save joe, blogPost, and comment, and save in Promise.all

--- test/association_test.js
+++ test/association_test.js
@@ -21,5 +21,9 @@ describe('Associations', () => {
     joe.blogPosts.push(blogPost);
     blogPost.comments.push(comment);
     comment.user = joe;
-  });
+
+    Promise.all([joe.save(), blogPost.save(), comment.save()])
+      .then(() => done());
+    });
+
 });
